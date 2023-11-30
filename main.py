6879d2642d271018edcb170289bd5355c1679fd0
MaximRakhmatullin <novyivek2000@gmail.com>
2023-11-30 20:51:05 +0500

    Added handling of input error

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 print('Генератор мемов запущен!')
 
-text_type = int(input("Введите 1, если нужен только нижний текст, и 2, если и верхний, и нижний: "))
+text_type = input("Введите 1, если нужен только нижний текст, и 2, если и верхний, и нижний: ")
 
 top_text = ''
 bottom_text = ''
 
-if text_type == 1:
+if text_type == '1':
     bottom_text = input('Введите нижний текст: ')
-elif text_type == 2:
+elif text_type == '2':
     top_text = input('Введите верхний текст: ')
     bottom_text = input('Введите нижний текст: ')
+else:
+    print('Введен неправильный режим. Перезапустите программу.')
+    quit()
 
 print(top_text, bottom_text)
